42f6f9e33254aae8ae1d373849761ddc9d1fa4a2
Cristobal Infantes <cinfantes@grupoasv.com>
2020-11-15 09:03:42 +0100

    adding persistence tests

---
+++ user/test/infrastructure/persistence/mongo-user-repository.test.js
@@ -0,0 +1,96 @@
+const MongoUserRepository = require('../../../infrastructure/persistence/mongo-user-repository');
+const User = require('../../../domain/user');
+const { toDomain } = require('../../../infrastructure/persistence/user-parser');
+
+describe('User mongo repository', () => {
+  let dbMock;
+  let mongoUserRepository;
+
+  beforeEach(() => {
+    dbMock = {
+      connect: jest.fn(),
+      disconnect: jest.fn()
+    };
+    mongoUserRepository = new MongoUserRepository({ db: dbMock });
+  });
+
+  test('should insert a user into collection', async () => {
+    const saveMock = jest.fn(() => Promise.resolve());
+    const collectionMock = {
+      collection: () => {
+        return { replaceOne: saveMock }
+      }
+    };
+
+    dbMock.connect.mockReturnValue(collectionMock);
+
+    const user = new User({
+      id: '1',
+      name: { firstName: 'firstName', surnames: 'surnames' },
+      info: { email: 'email', country: 'country', phone: 'phone', postalCode: 'postalCode' },
+      password:'password'
+    });
+    await mongoUserRepository.save(user);
+
+    const expectedUserDocument = {
+      _id: user.id,
+      name: user.name.firstName,
+      surnames: user.name.surnames,
+      email: user.info.email,
+      phone: user.info.phone,
+      country: user.info.country,
+      postalCode: user.info.postalCode,
+      password: user.password
+    };
+
+    expect(saveMock.mock.calls.length).toBe(1);
+    expect(saveMock.mock.calls[0][1]).toEqual(expectedUserDocument);
+  });
+
+  test('should return null if no user found by email', async () => {
+    const findOneMock = jest.fn(() => Promise.resolve());
+    const collectionMock = {
+      collection: () => {
+        return { findOne: findOneMock }
+      }
+    };
+    dbMock.connect.mockReturnValue(collectionMock);
+
+    const actualUser = await mongoUserRepository.findByEmail('email');
+
+    expect(findOneMock.mock.calls.length).toBe(1);
+    expect(findOneMock.mock.calls[0][0]).toEqual({ email: 'email' });
+    expect(actualUser).toEqual(null);
+  });
+
+  test('should return user found by email', async () => {
+    const userDocument = {
+      _id: '1',
+      name: 'firstName',
+      surnames: 'surnames',
+      email: 'email',
+      country: 'country',
+      phone: 'phone',
+      postalCode: 'postalCode',
+      password:'password'
+    };
+
+
+    const findOneMock = jest.fn(() => Promise.resolve(userDocument));
+    const collectionMock = {
+      collection: () => {
+        return { findOne: findOneMock }
+      }
+    };
+    dbMock.connect.mockReturnValue(collectionMock);
+
+    const actualUser = await mongoUserRepository.findByEmail('email');
+
+    expect(findOneMock.mock.calls.length).toBe(1);
+    expect(findOneMock.mock.calls[0][0]).toEqual({ email: 'email' });
+
+    const expectedUser = toDomain(userDocument);
+
+    expect(actualUser).toEqual(expectedUser);
+  });
+});
