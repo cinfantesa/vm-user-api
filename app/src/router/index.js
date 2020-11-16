cbb54e9cec1760d7b028e2095bab4a06097c0fb5
Cristobal Infantes <cinfantes@grupoasv.com>
2020-11-16 22:47:51 +0100

    adding routing and main pages

---
+++ app/src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '@/pages/login/Login';
+import Register from '@/pages/register/Register';
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('@/pages/home/Home')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
