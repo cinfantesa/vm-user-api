72ebabfd3f31ed0663ac6574fef42ca64fd49a46
Cristobal Infantes <cinfantes@grupoasv.com>
2020-11-21 17:11:47 +0100

    using sessionStorage

--- app/src/shared/session-service.js
+++ app/src/shared/session-service.js
@@ -8,26 +8,21 @@ export default class SessionService {
 
 
   loginUser(token) {
-    console.log('token', token)
     const decodedToken = jwtDecode(token)
-    console.log('decoded', decodedToken)
     this._loggedUser = new User(decodedToken)
-    console.log('user', this._loggedUser)
-    localStorage.setItem(TOKEN, token)
+    sessionStorage.setItem(TOKEN, token)
   }
 
   remove() {
-    localStorage.removeItem(TOKEN)
+    sessionStorage.removeItem(TOKEN)
     this._loggedUser = null
   }
 
   get user() {
-    console.log('getting user', this._loggedUser)
-    console.log('token in localStorage', localStorage.getItem(TOKEN))
     return this._loggedUser
   }
 
   get token() {
-    return localStorage.getItem(TOKEN)
+    return sessionStorage.getItem(TOKEN)
   }
 }
